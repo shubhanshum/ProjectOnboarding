fd29e556da0f040e6a7775970c889dc0c3feff7d
shubhanshum <shubhanshum22@gmail.com>
2021-02-08 12:56:47 +0530

    Adding array list code

---
+++ src/main/java/com/epam/collection/list/ArrayListImplementation.java
@@ -0,0 +1,13 @@
+package com.epam.collection.list;
+
+import java.util.List;
+
+public class ArrayListImplementation {
+
+	List<Integer> arrayList;
+
+	public List<Integer> displayNumbers(List<Integer> arrayList){	
+		return arrayList;
+	}
+	
+}
