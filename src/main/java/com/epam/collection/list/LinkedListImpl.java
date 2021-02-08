49e03822d9d832aaeb8a9559b84e76e6f2e32bfe
shubhanshum <shubhanshum22@gmail.com>
2021-02-08 13:07:13 +0530

    Adding linked list code

---
+++ src/main/java/com/epam/collection/list/LinkedListImpl.java
@@ -0,0 +1,10 @@
+package com.epam.collection.list;
+
+import java.util.List;
+
+public class LinkedListImpl {
+	
+	public List<String> printElements(List<String> linkedList){
+		return linkedList;
+	}
+}
